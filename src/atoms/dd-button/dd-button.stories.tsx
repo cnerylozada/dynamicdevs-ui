6e29cec8eb02e0e89316ed2adb29fcdd5091b42f
cnerylozada <cnery@dynamicdevs.io>
2022-02-14 17:44:51 -0500

    feat: storybook was implemented an addons for tailwind were installed

---
+++ src/atoms/dd-button/dd-button.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { DDButton } from "./index";
+
+export default {
+  title: "Example/Button",
+  component: DDButton,
+};
+
+const Template = (args: any) => <DDButton {...args} />;
+export const Primary = Template.bind({}) as any;
+Primary.args = { children: "#ff0" };
